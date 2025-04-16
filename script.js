a389c5f5d7fe8787b5c69d619e259704a539d234
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-04-16 12:53:18 +0300

    started adding JS lines

---
+++ script.js
@@ -0,0 +1,11 @@
+const taskInput = document.getElementById("task-input");
+console.log(taskInput)
+
+const addTaskBtn = document.getElementById("add-task");
+console.log(addTaskBtn)
+
+const  filterBtns = document.querySelectorAll(".filter-btn");
+console.log(filterBtns)
+
+const taskList = document.getElementById("task-list")
+console.log(taskList)
