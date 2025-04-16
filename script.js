b158b9ae569a9d65c35d1b165e097c06d235780e
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-04-16 13:54:18 +0300

    created first list and fucntion in JS, edited HTML & CSS

--- script.js
+++ script.js
@@ -4,8 +4,34 @@ console.log(taskInput)
 const addTaskBtn = document.getElementById("add-task");
 console.log(addTaskBtn)
 
-const  filterBtns = document.querySelectorAll(".filter-btn");
+const  filterBtns = document.querySelectorAll(".filter");
 console.log(filterBtns)
 
 const taskList = document.getElementById("task-list")
 console.log(taskList)
+
+
+
+let tasks = [];
+
+addTaskBtn.addEventListener("click", function() 
+{
+    const taskText = taskInput.value.trim();
+    if (taskText !==""){
+        tasks.push({ text: taskText});
+        taskInput.value ="";
+        showTasks();
+
+    }
+});
+
+function showTasks(){
+    taskList.innerHTML="";
+
+    tasks.forEach((task, index) =>
+        {
+        const li = document.createElement('li')
+        li.textContent = task.text;
+        taskList.appendChild(li);
+});
+}
